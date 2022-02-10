df95cc4ca99d34078c67d0cb1b37746b27f96aa0
antonseitz <aseitz@puckit.de>
2022-02-10 11:04:32 +0100

    Update get_xentool_ver.ps1

--- get_xentool_ver.ps1
+++ get_xentool_ver.ps1
@@ -29,7 +29,15 @@ if($line.DeviceName -like "*Bus*") {
 }
 
 
+$agent_citrix_ver=(Get-Command "c:\Program Files\Citrix\XenTools\xenguestagent.exe").FileVersionInfo.FileVersion
+
+$agent_xenserver_exe=(Get-WmiObject win32_service | ?{$_.Name -like 'xenagent'}).pathname
+# fileVersionraw  stat fileVersion, weil fileversion abweichend von der GUI nur 1.0 gebracht hat
+$agent_xenserver_ver=(Get-Command $agent_xenserver_exe).FileVersionInfo.ProductVersion
 
 
-"0 XenTools - OK - Agent: " + $agentver + " Xen Tools: " + $toolver + " (listed under APPS)"
-"0 XenToolsDriver - OK - Version: " + $outputshort + " (Details in long output)\n" + $outputlong 
+
+"0 XenTool - OK - Agent: " + $agentver + " Xen Tools: " + $toolver + " (listed under APPS)"
+"0 XenToolAgentCitrix - OK - " + "Citrix VM Tools Management Agent (xensrv) :" + $agent_citrix_ver
+"0 XenToolAgentXenserver - OK - " + "XenServer Agent (xenagent) :" + $agent_xenserver_ver
+"0 XenToolDriver - OK - Version: " + $outputshort + " (Details in long output)\n" + $outputlong
