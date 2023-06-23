77cfc106c9550af683d8c118a70f876d56355663
antonseitz <aseitz@puckit.de>
2023-06-23 15:54:56 +0200

    Update get_Tableau_status.ps1

--- win/get_Tableau_status.ps1
+++ win/get_Tableau_status.ps1
@@ -7,11 +7,11 @@
 # to activate caching you have to modify 
 ## C:\Program Files (x86)\checkmk\service\check_mk.yml
 
-    - pattern     : '$CUSTOM_LOCAL_PATH$\get_Tableau_status.ps1'           # in the local folder.
-          async       : yes             # activates caching
+#    - pattern     : '$CUSTOM_LOCAL_PATH$\get_Tableau_status.ps1'           # in the local folder.
+#          async       : yes             # activates caching
         #  timeout     : 60              # in seconds, after expiring timeout all processes matching the pattern will be killed.
-          run         : yes             # 
-          cache_age   : 300
+#          run         : yes             # 
+#          cache_age   : 300
 #
 #
 
@@ -31,9 +31,7 @@ $status_word="CRITICAL"	}
 
 
 
-# Umbrüche entfernen
-#$tsm_statussstatus=$tsm_status.replace("`r`n","")
-#$tsm_status
+
 
 
 
