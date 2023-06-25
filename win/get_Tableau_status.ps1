f4a4e7466d6bd7343c54d5623533d7ca9a7c1194
antonseitz <aseitz@puckit.de>
2023-06-25 08:37:20 +0200

    Update get_Tableau_status.ps1

--- win/get_Tableau_status.ps1
+++ win/get_Tableau_status.ps1
@@ -16,7 +16,7 @@
 #
 
 
-
+$ok=$halted=$degraded=0
 
 $tsm_status= tsm status -verbose
 
@@ -40,8 +40,33 @@ $tsm_match=$tsm_status -match 'Status:'
 $tsm_status=$tsm_status -replace '[^a-zA-Z ]','_'
 
 foreach($line in $tsm_status) {
+
+#$line
+
 $long_output += $line + "\n"
+
+$line_ok=$line -match 'wird ausgef_hrt'
+if($line_ok){ 
+$ok+=1
 }
 
 
-$status_nr + " Tableau_Status - " + $status_word + " - Tableau tsm status returns: " + $tsm_match + " (Details in long output)\n" +  $long_output 
+$line_halted=$line -match 'wurde angehalten'
+if($line_halted){
+$halted += 1
+}
+$line_degraded=$line -match 'fehlerhaften Zustand'
+if($line_degraded){
+$degraded += 1
+}
+
+#$ok #
+#$halted 
+#$degraded
+#
+}
+$hint="Manche Dienste sind angehalten, aber Status ist trotzdem RUNNING:\nDatenbankwartung,Sicherung_Wiederherstellung,Site_Import_Export\n\n"
+$all=$ok + $degraded + $halted
+$prozent=$ok/($halted + $ok + $degraded)
+$all=$ok + $halted + $degraded
+$status_nr + " Tableau_Status services=" + $prozent + ";.8;.9;0;1" + " Tableau TSM returns : " + $tsm_match + " Services: " + $ok + " of " + $all + " running, halted: "+ $halted + ", degraded:" + $degraded + " (Details in long output)\n" + $hint +   $long_output 
