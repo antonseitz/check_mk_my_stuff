409aaff4c72215ef154f94778e9461b5d1fed028
antonseitz <aseitz@puckit.de>
2023-09-13 16:37:41 +0200

    Update get_users_logged_in.ps1

--- win/get_users_logged_in.ps1
+++ win/get_users_logged_in.ps1
@@ -1,1 +1,3 @@
-"0 VirtioDriverVersion - OK - Version: " + $outputshort + " (Details in long output)\n" + $outputlong
+$outputshort = " (Details in long output)" 
+$outputlong = " (Details)" 
+"0 Users_logged_in - OK - Version: " + $outputshort + " (Details in long output)\n" + $outputlong
