4a2d8a301d9d74f4c42401fcc14190e23dddfc1e
antonseitz <aseitz@puckit.de>
2023-09-26 15:53:34 +0200

    Update activate_plugin.ps1

--- win/activate_plugin.ps1
+++ win/activate_plugin.ps1
@@ -1,6 +1,10 @@
 get-childitem (${env:ProgramFiles(x86)} + "\checkmk\service\plugins" )
 
-"which one to activate?"
+"
+Which plugin from " + ${env:ProgramFiles(x86)} + "\checkmk\service\plugins do you want to activate?
+Symlink in PROGRAMDATA\checkm\agent\plugins\ will be created
+Type filename from list above here: 
+"
 
 $plugin= read-host
 New-Item -target (${env:ProgramFiles(x86)} + "\checkmk\service\plugins\" + $plugin) -ItemType SymbolicLink -path ..\..\plugins\$plugin
